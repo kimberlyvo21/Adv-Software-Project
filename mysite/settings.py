6a5540bdedb46641199eb54858a1daa18e901bb9
Hayden Sarpong <Hos5hh@virginia.edu>
2021-02-18 22:43:12 -0500

    changing commit file

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '*x&_bk^k*(&kb4z6y^#6n=ioas9syc+d5h%^hoz39)vt)6q6z#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["a-09.herokuapp.com", "project-a-09.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0","127.0.0.1","a-09.herokuapp.com", "project-a-09.herokuapp.com"]
 
 
 # Application definition
