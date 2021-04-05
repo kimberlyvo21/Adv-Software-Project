045fd39c8415a9150dec52e2a1efe55e78892da3
haris433 <61633837+haris433@users.noreply.github.com>
2021-04-04 22:18:10 -0400

    Update tests.py

--- social_app/tests.py
+++ social_app/tests.py
@@ -1,10 +1,23 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 # Create your tests here.
 
 
-class DummyTestCase(TestCase):
-    def setUp(self):
-        x = 1
+def create_account(username, age, height, workout_time):
+    User.objects.create_user(username=username, email="John@gmail.com")
+    return Profile.objects.create(User=User.objects.get(username=username), name="John Doe", age=age, height=height, time=workout_time,
+                                  email="John@gmail.com")
 
-    def test_dummy_test_case(self):
-        self.assertEqual(1, 1)
+
+class ProfileCreation(TestCase):
+    def test_case_profile_accept(self):
+        new_user = create_account("James", 18, 6, 60)
+        all_users = User.objects.all()
+        self.assertQuerysetEqual(all_users, ['<User: James>'])
+
+    def test_case_profile_more_than_one(self):
+        new_user = create_account("James", 18, 6, 60)
+        new_user2 = create_account("John", 19, 7, 70)
+        all_users = User.objects.all()
+        self.assertQuerysetEqual(all_users, ['<User: James>', '<User: John>'], ordered=False)
