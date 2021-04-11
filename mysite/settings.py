fd93ed414dd97d2563f4a590bdd7ab0810ecf6f5
Hayden Sarpong <Hos5hh@virginia.edu>
2021-04-11 17:27:10 -0400

    Travis Permission Fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': 'mydb',
         'USER': 'myuser',
         'PASSWORD': 'mypass',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '5342',
     }
 }
